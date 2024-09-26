9c48b58f6fd21542211b38eb3e8104b63a30c34c
LukeIsCringe <lukejwatts05@gmail.com>
2024-09-26 11:32:08 +0100

    Initial commit

---
+++ BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1_CET254
+{
+    internal class BinarySearch
+    {
+        public static int Perform(double searchValue, List<double> listToSearch)
+        {
+            int left = 0;
+            int right = listToSearch.Count - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (listToSearch[mid] == searchValue)
+                {
+                    return mid; // Target found at index mid
+                }
+                else if (listToSearch[mid] < searchValue)
+                {
+                    left = mid + 1; // Search in the right half
+                }
+                else
+                {
+                    right = mid - 1; // Search in the left half
+                }
+            }
+
+            return -1; //Target not found
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("W1_CET254")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("University of Sunderland")]
+[assembly: AssemblyProduct("W1_CET254")]
+[assembly: AssemblyCopyright("Copyright © University of Sunderland 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f10f723-6528-4ed3-b1a5-005659e0ad35")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
